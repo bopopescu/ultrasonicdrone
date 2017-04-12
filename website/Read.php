27fdb0fcb93dc9dc7db6b5b71ede393ffca51d19
Haley Alexander <hal.00.alexander@gmail.com>
2017-04-12 14:49:02 -0500

    Update Read.php

--- website/Read.php
+++ website/Read.php
@@ -1,19 +1,31 @@
 <?php 
 function getLast(){ 
 	//connect
-	$conn = mysqli_connect('localhost', 'root', 'ultra', 'drone_data') or die ("Error");
-	
+	$conn = mysql_connect('localhost', 'root', 'ultra', 'drone_data');
+	if(!$conn){
+	die ('Error');
+	mysqli_close($conn);
+	echo ('die'); 
+	exit; 
+	} 	
+	$db = mysql_select_db("data", $conn); 
+	if(!$db){
+	die('fail');
+	mysql_close($conn);
+	exit; 
+	}
 	//get
         $find = "SELECT' 'height' FROM 'data' ORDER BY 'time' DESC LIMIT 1"
-	$query = mysqli_query($conn, $find) or die ("Error");
+	$query = mysql_query($find, $conn);
 	
 	if($query){
 	echo($query); 
-	mysqli_close($conn);
+	mysql_close($conn);
         exit; 
 	} 
 	else{
-	echo "Error";  
+	echo ('Error');  
+	mysql_close($conn);
 	exit;
 	}  
 	
