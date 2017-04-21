17efa7df2872ae186fc5cede21c8a29d1408f2b4
Arnav Kaul <akkaul1195@gmail.com>
2017-04-20 21:13:28 -0500

    added final control file

---
+++ python/final_control.py
@@ -0,0 +1,47 @@
+#/bin/sys/python
+from __future__ import print_function
+from decimal import Decimal
+from datetime import datetime, date, timedelta
+import time
+import mysql.connector
+import random
+
+
+cnx = mysql.connector.connect(user='root', password = 'ultra', host = 'local host', database='employees')
+
+# Get two cursors
+curA = cnx.cursor(buffered=True)
+curB = cnx.cursor(buffered=True)
+
+#vars
+date = str(datetime.now())
+height = float((40 * int(10)) * (343/2))/1000000
+speed = .12
+fifo = 10
+
+
+while(true):
+	direction = random.randint(0,1)
+	dif = random.randint(0,7)
+	if(direction == 0):
+		if(fifo >= dif):
+			fifo = fifo - dif
+	else:
+		if(fifo+dif > 767):
+			fifo = 767
+		else:
+			fifo = fifo + dif
+	oldheight = height
+	height = float((40 * int(fifo)) * (343/2))/1000000
+	speed = float((height-oldheight)/.3)
+	date = str(datetime.now())
+	# Insert command 
+	query = ("INSERT INTO data VALUES (%s, %s, %s)", (date, height, speed))
+	curA.execute(query)
+
+
+	# Commit the changes
+	cnx.commit()
+	time.sleep(.3)
+
+cnx.close()
