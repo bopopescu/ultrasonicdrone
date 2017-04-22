7764f0aa62d4ac8c3cb990f428cfdd5e667a663d
Haley Alexander <hal.00.alexander@gmail.com>
2017-04-22 13:20:54 -0500

    Add files via upload

---
+++ website/ReadSpeed.php
@@ -0,0 +1,23 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "ultra";
+$dbname = "drone_data";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+$sql = "SELECT * FROM data ORDER BY time DESC LIMIT 1";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo json_encode(floatval($row["speed"]));
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
